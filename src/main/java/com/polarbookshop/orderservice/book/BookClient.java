6e2c863a33e9be0255b5b03aef9276293fe86fc3
Marko Korovin <markondopolo@gmail.com>
2025-06-12 11:57:07 +0200

    add fallbaks & error handling for WebClient connection

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -2,6 +2,7 @@ package com.polarbookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -23,8 +24,11 @@ public class BookClient {
                 .retrieve()
                 .bodyToMono(Book.class)
                 .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        ex -> Mono.empty())
                 .retryWhen(
-                        Retry.backoff(3, Duration.ofMillis(100))
-                );
+                        Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class,
+                        ex -> Mono.empty());
     }
 }
